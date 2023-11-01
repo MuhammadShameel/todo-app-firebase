86d868e41b655ba9d2d470d4a4cc45cbf1b5d49b
MuhammadShameel <mshameelkz@gmail.com>
2023-11-01 20:43:19 +0500

    functionality for todo list has done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { dataBase } from "./firebase.utils";
 import firebase from "firebase/compat/app";
 
 function App() {
   const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    dataBase
+      .collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            todo: doc.data().todo,
+            timestamp: doc.data().timestamp,
+          }))
+        );
+      });
+  }, []);
+
+  console.log(todos);
 
   const addTodo = (e) => {
     e.preventDefault();
@@ -18,7 +36,7 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <h1>TODO LIST</h1>
+        <h1>TODO APP</h1>
         <form className="input-group mb-3">
           <input
             type="text"
@@ -26,32 +44,60 @@ function App() {
             onChange={(e) => setInput(e.target.value)}
             value={input}
           />
-          <button type="submit" className="btn btn-primary" onClick={addTodo}>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={addTodo}
+            disabled={!input}
+          >
             ADD TASK
           </button>
         </form>
       </div>
       <div className="card">
-        <table className="table">
+        <table className="table table-hover">
           <thead>
             <tr>
               <th scope="col">ID</th>
               <th scope="col">TODO</th>
-              <th scope="col"></th>
-              <th scope="col"></th>
+              <th scope="col">ACTION</th>
+              <th scope="col">ACTION</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <th>Hello</th>
-              <th>
-                <button className="btn btn-success">Update</button>
-              </th>
-              <th>
-                <button className="btn btn-danger">Delete</button>
-              </th>
-            </tr>
+            {todos.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <th>{todo.id}</th>
+                  <th>{todo.todo}</th>
+                  <th>
+                    <button
+                      className="btn btn-success"
+                      disabled={!input}
+                      onClick={() => {
+                        dataBase.collection("todos").doc(todo.id).update({
+                          todo: input,
+                          timestamp:
+                            firebase.firestore.FieldValue.serverTimestamp(),
+                        });
+                      }}
+                    >
+                      Update
+                    </button>
+                  </th>
+                  <th>
+                    <button
+                      className="btn btn-danger"
+                      onClick={(e) =>
+                        dataBase.collection("todos").doc(todo.id).delete()
+                      }
+                    >
+                      Remove
+                    </button>
+                  </th>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
